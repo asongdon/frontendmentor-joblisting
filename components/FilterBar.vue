d0a19d1f8e5aad611d810fed82fab873d0ffbb4a
Art-ong Songdon <a.songdon@gmail.com>
2020-04-13 11:50:23 +0200

    styling filterbar

--- components/FilterBar.vue
+++ components/FilterBar.vue
@@ -3,7 +3,9 @@
     class="mb-10 -mt-16 shadow-lg bg-white px-5 py-4 rounded flex justify-between items-center"
   >
     <!-- tags -->
-    <div class="flex flex-wrap justify-start lg:justify-end lg:items-center mt-2">
+    <div
+      class="flex flex-wrap justify-start lg:justify-end lg:items-center mt-2"
+    >
       <div class="flex justify-start mr-3 mb-2">
         <div
           class="flex px-2 py-1 bg-gray-200 text-gray-500 rounded-l font-medium text-sm"
@@ -11,19 +13,7 @@
           Frontend
         </div>
         <div
-          class="bg-teal-500 px-2 py-1 rounded-r text-white cursor-pointer font-bold"
-        >
-          ✕
-        </div>
-      </div>
-      <div class="flex justify-start mr-3 mb-2">
-        <div
-          class="flex px-2 py-1 bg-gray-200 text-gray-500 rounded-l font-medium text-sm"
-        >
-          Frontend
-        </div>
-        <div
-          class="bg-teal-500 px-2 py-1 rounded-r text-white cursor-pointer font-bold"
+          class="bg-teal-500 px-2 py-1 rounded-r text-white cursor-pointer font-bold hover:bg-gray-900"
         >
           ✕
         </div>
