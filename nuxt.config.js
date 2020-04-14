427fbb377dd113dc363a9b95b49feb48c5fd4a7d
Art-ong Songdon <a.songdon@gmail.com>
2020-04-15 01:31:37 +0200

    fixing typo

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,7 +2,7 @@ const routerBase =
   process.env.DEPLOY_ENV === 'GH_PAGES'
     ? {
         router: {
-          base: '/frontendmentor-joblisting>/'
+          base: '/frontendmentor-joblisting/'
         }
       }
     : {}
